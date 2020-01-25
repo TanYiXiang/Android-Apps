216acf3d85a48d9b14b0bd44695a7e79b046bf72
Daryl Tan <tanyixiang16@yahoo.com>
2020-01-25 23:22:39 +0800

    Completed win checks for horizontal and vertical tiles. Completed reset button functionality. Improved button logic code structure.

--- Tic Tac Toe/app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ Tic Tac Toe/app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -5,40 +5,97 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     var playerOneTurn = true
-    var playerTwoTurn = false
+    var roundCount = 0
     lateinit var gameText: TextView
+    lateinit var resetButton: Button
+    private lateinit var tileButtons: ArrayList<Button>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         gameText = findViewById(R.id.game_text)
+        resetButton = findViewById(R.id.reset_button)
+        tileButtons = arrayListOf()
         for (i in 0..8) {
             val idString = "button_$i"
             val buttonId = getResources().getIdentifier(idString, "id", packageName)
-            findViewById<Button>(buttonId).setOnClickListener { buttonClicked(it) }
+            tileButtons.add(findViewById(buttonId))
+            tileButtons[i].setOnClickListener { gameTileClicked(it) }
         }
+
+        resetButton.setOnClickListener { resetGame(tileButtons) }
     }
 
-    private fun buttonClicked(view: View) {
+    private fun gameTileClicked(view: View) {
+        val button = view as Button
+        if (view.text != "") {
+            Toast.makeText(this, R.string.tileAlreadySelected, Toast.LENGTH_SHORT).show()
+            return
+        }
 
-       val button = view as Button
-
+        //Set tile icons
         if (playerOneTurn) {
             view.text = "O"
-            playerOneTurn = false
-            playerTwoTurn = true
             gameText.text = getString(R.string.playerTwoTurn)
         } else {
             button.text = "X"
-            playerOneTurn = true
-            playerTwoTurn = false
             gameText.text = getString(R.string.playerOneTurn)
         }
+
+        roundCount += 1
+
+        //Check for winning combinations
+        if (checkWin(tileButtons)) {
+            if (playerOneTurn) {
+                gameText.text = getString(R.string.playerOneWins)
+            } else {
+                gameText.text = getString(R.string.playerTwoWins)
+            }
+        } else if (roundCount == 9) {
+            gameText.text = getString(R.string.playersDraw)
+        } else {
+            playerOneTurn = !playerOneTurn
+        }
+
+    }
+
+    private fun checkWin(tileButtons: ArrayList<Button>): Boolean {
+
+        //Check horizontal tiles
+        for (i in 0..8 step 3) {
+            if ((tileButtons[i].text == tileButtons[i + 1].text) && (tileButtons[i].text == tileButtons[i + 2].text)
+                && (tileButtons[i].text != "")
+            ) {
+                return true
+            }
+        }
+
+        //Check vertical tiles
+        for (i in 0..2){
+            if ((tileButtons[i].text == tileButtons[i + 3].text) && (tileButtons[i].text == tileButtons[i + 6].text)
+                && (tileButtons[i].text != "")
+            ) {
+                return true
+            }
+        }
+        return false
+    }
+
+
+    private fun resetGame(tileButtons: ArrayList<Button>) {
+        for (buttons in tileButtons) {
+            buttons.text = ""
+        }
+        roundCount = 0
+        playerOneTurn = true
+        gameText.text = getString(R.string.playerOneTurn)
+        Toast.makeText(this, R.string.gameReset, Toast.LENGTH_SHORT).show()
 
     }
 
