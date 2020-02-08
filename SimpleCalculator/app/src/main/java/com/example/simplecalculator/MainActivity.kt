ac66942d73a218296908361f81826b778443dbab
Daryl Tan <tanyixiang16@yahoo.com>
2020-02-09 00:05:29 +0800

    Fix bug where data loss occur due to config changes.

--- SimpleCalculator/app/src/main/java/com/example/simplecalculator/MainActivity.kt
+++ SimpleCalculator/app/src/main/java/com/example/simplecalculator/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.simplecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.simplecalculator.databinding.ActivityMainBinding
@@ -23,31 +24,54 @@ class MainActivity : AppCompatActivity() {
         initExpressionButtonClickListeners()
     }
 
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        expression_text.text = savedInstanceState?.getString("ExpressionText")
+        result_text.text = savedInstanceState?.getString("ResultText")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString("ExpressionText", expression_text.text.toString())
+        outState.putString("ResultText", result_text.text.toString())
+        super.onSaveInstanceState(outState)
+    }
+
     private fun initNumberButtonClickListeners() {
         binding.apply {
-            button_one.setOnClickListener { appendExpression(buttonOne.text) }
-            button_two.setOnClickListener { appendExpression(buttonTwo.text) }
-            button_three.setOnClickListener { appendExpression(buttonThree.text) }
-            button_four.setOnClickListener { appendExpression(buttonFour.text) }
-            button_five.setOnClickListener { appendExpression(buttonFive.text) }
-            button_six.setOnClickListener { appendExpression(buttonSix.text) }
-            button_seven.setOnClickListener { appendExpression(buttonSeven.text) }
-            button_eight.setOnClickListener { appendExpression(buttonEight.text) }
-            button_nine.setOnClickListener { appendExpression(buttonNine.text) }
-            zero_button.setOnClickListener { appendExpression(zeroButton.text) }
-            decimal_button.setOnClickListener { appendExpression(decimalButton.text) }
+            button_one.setOnClickListener { appendExpression(buttonOne.text, true) }
+            button_two.setOnClickListener { appendExpression(buttonTwo.text, true) }
+            button_three.setOnClickListener { appendExpression(buttonThree.text, true) }
+            button_four.setOnClickListener { appendExpression(buttonFour.text, true) }
+            button_five.setOnClickListener { appendExpression(buttonFive.text, true) }
+            button_six.setOnClickListener { appendExpression(buttonSix.text, true) }
+            button_seven.setOnClickListener { appendExpression(buttonSeven.text, true) }
+            button_eight.setOnClickListener { appendExpression(buttonEight.text, true) }
+            button_nine.setOnClickListener { appendExpression(buttonNine.text, true) }
+            zero_button.setOnClickListener { appendExpression(zeroButton.text, true) }
+            decimal_button.setOnClickListener { appendExpression(decimalButton.text, true) }
 
         }
     }
 
     private fun initExpressionButtonClickListeners() {
         binding.apply {
-            left_parentheses_button.setOnClickListener { appendExpression(left_parentheses_button.text) }
-            right_parentheses_button.setOnClickListener { appendExpression(rightParenthesesButton.text) }
-            plus_button.setOnClickListener { appendExpression(plus_button.text) }
-            subtract_button.setOnClickListener { appendExpression(subtract_button.text) }
-            multiply_button.setOnClickListener { appendExpression(multiply_button.text) }
-            division_button.setOnClickListener { appendExpression(division_button.text) }
+            left_parentheses_button.setOnClickListener {
+                appendExpression(
+                    left_parentheses_button.text,
+                    false
+                )
+            }
+            right_parentheses_button.setOnClickListener {
+                appendExpression(
+                    rightParenthesesButton.text,
+                    false
+                )
+            }
+            plus_button.setOnClickListener { appendExpression(plus_button.text, false) }
+            subtract_button.setOnClickListener { appendExpression(subtract_button.text, false) }
+            multiply_button.setOnClickListener { appendExpression(multiply_button.text, false) }
+            division_button.setOnClickListener { appendExpression(division_button.text, false) }
             back_button.setOnClickListener { backExpression() }
             CE_button.setOnClickListener { clearExpression() }
             equal_button.setOnClickListener { calculateExpression() }
@@ -71,15 +95,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun appendExpression(buttonText: CharSequence) {
+    private fun appendExpression(buttonText: CharSequence, isNumber: Boolean) {
 
-        if(result_text.text.isNotEmpty()){
+        if (result_text.text.isNotEmpty()) {
             expression_text.text = ""
         }
 
-        binding.expressionText.append(result_text.text.toString())
-        binding.expressionText.append(buttonText.toString())
-        result_text.text = ""
+        if (isNumber) {
+            result_text.text = ""
+            expression_text.append(buttonText.toString())
+        } else {
+            binding.expressionText.append(result_text.text.toString())
+            binding.expressionText.append(buttonText.toString())
+            result_text.text = ""
+        }
 
     }
 
