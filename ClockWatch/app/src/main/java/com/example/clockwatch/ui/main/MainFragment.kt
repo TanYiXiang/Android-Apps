a72e7f28ca38d3d9c63dbb3b503046a31fdbdc91
Daryl Tan <tanyixiang16@yahoo.com>
2020-02-19 00:02:16 +0800

    Completed clock fragment layout and functionality

---
+++ ClockWatch/app/src/main/java/com/example/clockwatch/ui/main/MainFragment.kt
@@ -0,0 +1,47 @@
+package com.example.clockwatch.ui.main
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.clockwatch.R
+import com.example.clockwatch.databinding.MainFragmentBinding
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+    private lateinit var binding:MainFragmentBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = DataBindingUtil.inflate(inflater,R.layout.main_fragment,container,false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        viewModel.timeZoneString.observe(viewLifecycleOwner, Observer { currentTimeZone->
+            binding.locationText.text = currentTimeZone.toString()
+        })
+
+        viewModel.dateString.observe(viewLifecycleOwner, Observer { currentDate->
+            binding.dateText.text = currentDate.toString()
+        })
+
+
+    }
+
+
+}
---
+++ ClockWatch/app/src/main/java/com/example/clockwatch/ui/main/MainViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.clockwatch.ui.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+class MainViewModel : ViewModel() {
+
+    private val _timeZoneString = MutableLiveData<String>()
+    val timeZoneString: LiveData<String>
+        get() = _timeZoneString
+
+    private val _dateString = MutableLiveData<String>()
+    val dateString: LiveData<String>
+        get() = _dateString
+
+    init {
+        _timeZoneString.value = TimeZone.getDefault().id.toString()
+        _dateString.value = getDate()
+    }
+
+    private fun getDate(): String {
+        val date = LocalDateTime.now()
+        val dayOfWeek = date.dayOfWeek.toString()
+        val formatter = DateTimeFormatter.ofPattern(" dd-MM-yyyy ")
+
+        return  dayOfWeek + formatter.format(date)
+    }
+
+}
